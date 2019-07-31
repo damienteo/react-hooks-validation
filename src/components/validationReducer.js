85429a297a230d454b7ae0b9fb2cb4ab4d3ea514
damienteo <ksteo89@gmail.com>
2019-07-31 15:01:02 +0800

    Set up validationReducer

---
+++ src/components/validationReducer.js
@@ -0,0 +1,20 @@
+const initialState = {
+  values: {},
+  errors: {},
+  submitted: false
+};
+
+export function validationReducer(state, action) {
+  switch (action.type) {
+    case "change":
+      const values = { ...state.values, ...action.payload };
+      return {
+        ...state,
+        values
+      };
+    case "submit":
+      return { ...state, submitted: true };
+    default:
+      throw newError("Unknown action type");
+  }
+}
